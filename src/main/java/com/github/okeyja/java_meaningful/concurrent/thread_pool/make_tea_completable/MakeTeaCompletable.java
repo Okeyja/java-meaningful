5eac0139ad361dcc0e6b4e95c27718787e1d8b5a
Okeyja Teung <dengyijian163@163.com>
2021-02-14 15:10:06 +0800

    feature: CompletableFuture reletionships.

--- src/main/java/com/github/okeyja/java_meaningful/concurrent/thread_pool/make_tea_completable/MakeTeaCompletable.java
+++ src/main/java/com/github/okeyja/java_meaningful/concurrent/thread_pool/make_tea_completable/MakeTeaCompletable.java
@@ -3,6 +3,22 @@ package com.github.okeyja.java_meaningful.concurrent.thread_pool.make_tea_comple
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * // 如果所有 CompletableFuture 共享一个线程池，那么一旦有任务执行一些很慢的 I/O 操作，
+ * // 就会导致线程池中所有线程都阻塞在 I/O 操作上，从而造成线程饥饿，进而影响整个系统的性能。
+ * // 所以，强烈建议你要根据不同的业务类型创建不同的线程池，以避免互相干扰。
+ * <p>
+ * // 使用默认线程池
+ * static CompletableFuture&lt;Void&gt;
+ * runAsync(Runnable runnable)
+ * static &lt;U&gt; CompletableFuture&lt;U&gt;
+ * supplyAsync(Supplier&lt;U&gt; supplier)
+ * // 可以指定线程池
+ * static CompletableFuture&lt;Void&gt;
+ * runAsync(Runnable runnable, Executor executor)
+ * static &lt;U&gt; CompletableFuture&lt;U&gt;
+ * supplyAsync(Supplier&lt;U&gt; supplier, Executor executor)
+ */
 public class MakeTeaCompletable {
     public static void main(String[] args) {
         // 任务1：洗水壶->烧开水，没有返回值
@@ -26,6 +42,39 @@ public class MakeTeaCompletable {
             return "龙井";
         });
         // 任务3：任务1和任务2完成后执行：泡茶
+        // 串行关系：thenApply、thenAccept、thenRun 和 thenCompose
+        // thenApply 支持返回值
+        // thenAccept 不支持返回值
+        // thenRun 不支持返回值 Runnable
+        // thenCompose 支持返回值 创建新的子流程，结果同 thenApply
+        /*
+            CompletionStage<R> thenApply(fn);
+            CompletionStage<R> thenApplyAsync(fn);
+            CompletionStage<Void> thenAccept(consumer);
+            CompletionStage<Void> thenAcceptAsync(consumer);
+            CompletionStage<Void> thenRun(action);
+            CompletionStage<Void> thenRunAsync(action);
+            CompletionStage<R> thenCompose(fn);
+            CompletionStage<R> thenComposeAsync(fn);
+         */
+        // AND 汇聚关系
+        /*
+            CompletionStage<R> thenCombine(other, fn);
+            CompletionStage<R> thenCombineAsync(other, fn);
+            CompletionStage<Void> thenAcceptBoth(other, consumer);
+            CompletionStage<Void> thenAcceptBothAsync(other, consumer);
+            CompletionStage<Void> runAfterBoth(other, action);
+            CompletionStage<Void> runAfterBothAsync(other, action);
+         */
+        // OR 汇聚关系
+        /*
+            CompletionStage applyToEither(other, fn);
+            CompletionStage applyToEitherAsync(other, fn);
+            CompletionStage acceptEither(other, consumer);
+            CompletionStage acceptEitherAsync(other, consumer);
+            CompletionStage runAfterEither(other, action);
+            CompletionStage runAfterEitherAsync(other, action);
+         */
         CompletableFuture<String> f3 = f1.thenCombine(f2, (__, tf) -> {
             System.out.println("T1:拿到茶叶:" + tf);
             System.out.println("T1:泡茶...");
