20e8b811c3d0250216195e0f78659b954e5a5615
Okeyja Teung <dengyijian163@163.com>
2021-02-18 14:28:42 +0800

    save: sample make-tea in one java file.

--- src/main/java/com/github/okeyja/java_meaningful/concurrent/thread_pool/make_tea/MakeTea.java
+++ src/main/java/com/github/okeyja/java_meaningful/concurrent/thread_pool/make_tea/MakeTea.java
@@ -1,9 +1,51 @@
 package com.github.okeyja.java_meaningful.concurrent.thread_pool.make_tea;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 public class MakeTea {
+
+    private static class T1Task implements Callable<String> {
+        private final FutureTask<String> ft2;
+
+        // T1任务需要T2任务的FutureTask
+        T1Task(FutureTask<String> ft2) {
+            this.ft2 = ft2;
+        }
+
+        @Override
+        public String call() throws Exception {
+            System.out.println("T1:洗水壶...");
+            TimeUnit.SECONDS.sleep(1);
+
+            System.out.println("T1:烧开水...");
+            TimeUnit.SECONDS.sleep(15);
+            // 获取 T2 线程的茶叶
+            String tf = ft2.get();
+            System.out.println("T1:拿到茶叶:" + tf);
+
+            System.out.println("T1:泡茶...");
+            return "上茶:" + tf;
+        }
+    }
+
+    private static class T2Task implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            System.out.println("T2:洗茶壶...");
+            TimeUnit.SECONDS.sleep(1);
+
+            System.out.println("T2:洗茶杯...");
+            TimeUnit.SECONDS.sleep(2);
+
+            System.out.println("T2:拿茶叶...");
+            TimeUnit.SECONDS.sleep(1);
+            return "龙井";
+        }
+    }
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 创建任务T2的FutureTask
         FutureTask<String> ft2 = new FutureTask<>(new T2Task());
